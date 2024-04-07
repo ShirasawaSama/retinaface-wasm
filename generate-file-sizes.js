6754c976b0ce854dd7e50a737aa36d0a8602c031
Shirasawa <764798966@qq.com>
2024-04-07 18:23:53 +0800

    Update

---
+++ generate-file-sizes.js
@@ -0,0 +1,22 @@
+import fs from 'fs/promises'
+
+const json = { }
+const process = async dir => {
+  const files = await fs.readdir(dir)
+  for (const file of files) {
+    const path = `${dir}/${file}`
+    const stat = await fs.stat(path)
+    if (stat.isDirectory()) {
+      await process(path)
+    } else {
+      json[path] = stat.size
+    }
+  }
+}
+;(async () => {
+  await Promise.all([
+    process('wasm'),
+    process('models')
+  ])
+  await fs.writeFile('files.json', JSON.stringify(json))
+})()
